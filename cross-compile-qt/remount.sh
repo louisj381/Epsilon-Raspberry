0766557b74b7e126b9801b94e5218d2ed7fbee73
Enoch Tsang <etsang1@hotmail.com>
2016-10-11 20:48:18 -0600

    update to compile xcb platform as well

--- cross-compile-qt/remount.sh
+++ cross-compile-qt/remount.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 sudo mount -o loop,offset=70254592 2016-05-27-raspbian-jessie-cross-compile-qt.img /mnt/rasp-pi-rootfs
+# sudo mount -o loop,offset=70254592 /dev/mmcblk0 /mnt/rasp-pi-rootfs
