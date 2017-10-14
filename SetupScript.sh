28cf46c6f13242688010f1e8d9545550ac09f7e2
louisj381 <louisj381@gmail.com>
2017-10-14 13:44:55 -0600

    added commands to clone raspberry repo and move primary files to appropriate directories

--- SetupScript.sh
+++ SetupScript.sh
@@ -2,3 +2,9 @@
 
 ###base script to fresh install systems onto base image for raspberry pi
 
+#configure displays
+cd ~
+git clone https://github.com/UCSolarCarTeam/Epsilon-Raspberry.git
+cd Epsilon-Raspberry/primary
+sudo cp xorg.conf /etc/X11/
+sudo cp config.txt /boot/
