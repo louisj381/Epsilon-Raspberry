526b0786dbfe627a1ab0a377acce5d93efc2b024
louisj381 <louisj381@gmail.com>
2017-10-17 20:48:37 -0600

    added rabbit-mq installation files, cloned dashboard to the pi

--- SetupScript.sh
+++ SetupScript.sh
@@ -6,9 +6,12 @@
 cd ~
 git clone https://github.com/UCSolarCarTeam/Epsilon-Raspberry.git
 cd Epsilon-Raspberry
-#switch to xorgfix branch
-git checkout xorgfix
 cd primary/
 sudo cp xorg.conf /etc/X11/
 sudo cp config.txt /boot/
+#install rabbit-mq
+cd ~
+git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git
+cd Epsilon-Dashboard/
+yes | ./EpsilonDashboardSetup.sh #continue through without inturruption
 
