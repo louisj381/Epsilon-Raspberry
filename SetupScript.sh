898e63dfbf329361515e732cec4034da5e8a7277
louisj381 <louisj381@gmail.com>
2017-10-29 12:09:41 -0600

    changed script to require sudo, removed install script content, added rabbitmq install and the beggining of QT installation

--- SetupScript.sh
+++ SetupScript.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+sudo: required
 ###base script to fresh install systems onto base image for raspberry pi
 
 #configure displays
 cd ~
 git clone https://github.com/UCSolarCarTeam/Epsilon-Raspberry.git
-cd Epsilon-Raspberry
-cd primary/
-sudo cp xorg.conf /etc/X11/
-sudo cp config.txt /boot/
+cd Epsilon-Raspberry/primary/
+cp xorg.conf /etc/X11/
+cp config.txt /boot/
 #install rabbit-mq
-cd ~
-git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git
-cd Epsilon-Dashboard/
-yes | ./EpsilonDashboardSetup.sh #continue through without inturruption
 
+echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list && sudo apt-get update && sudo apt-get install rabbitmq-server
+sudo apt-get install cmake libboost-dev openssl libssl-dev libblkid-dev e2fslibs-dev libboost-all-dev libaudit-dev software-properties-common build-essential mesa-common-dev libgl1-mesa-dev
+
+cd /tmp/
+if ls /usr/local/lib/librabbitmq.* 1> /dev/null 2>&1 ;
+then
+echo "Rabbitmq already setup"
+else
+git clone https://github.com/alanxz/rabbitmq-c
+mkdir rabbitmq-c/build && cd rabbitmq-c/build
+cmake ..
+cmake --build .
+cp librabbitmq/*.a /usr/local/lib/
+cp librabbitmq/*.so* /usr/local/lib/
+fi
+
+#begin QT install
+apt-get update
+yes | apt-get upgrade
+ 
