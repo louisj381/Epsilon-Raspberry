24094a6feeee3f1b46341e04ba7d8674d3520f01
louisj381 <louisj381@gmail.com>
2018-01-04 23:00:36 -0700

    Updated Scripts in unison with SetupScript, used absolute paths and fixed naming in comments

--- PrimaryInstallScript.sh
+++ PrimaryInstallScript.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-if [ $USER -ne "root"]
- 	then
- 	echo "Permission Denied"
- 	exit 1
+if [ $EUID -ne 0 ]; then
+	echo "Please run as root user"
+	exit 1
 fi
 
 ping -q -c1 google.com > /dev/null
  
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
 	echo "No Internet Connection...Aborting"
 	exit 1
 fi
 
 #install Epsilon-Hermes
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/Epsilon-Hermes.git)
-(cd /usr/local/Epsilon-Hermes && cd ../ && mv ./Epsilon-Hermes ./src && mkdir Epsilon-Hermes && mv ./src ./Epsilon-Hermes/)
+git clone https://github.com/UCSolarCarTeam/Epsilon-Hermes.git /opt/
+(cd /opt/Epsilon-Hermes && cd ../ && mv ./Epsilon-Hermes ./src && mkdir Epsilon-Hermes && mv ./src ./Epsilon-Hermes/)
 
-#install Epsilon-Onboard-Media-Control
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/BackupCamera.git)
-cd /usr/local/BackupCamera/Installer
-./MainInstaller.sh
-./AutoBootSetup.sh
+#install BackupCamera
+git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/
+/opt/BackupCamera/Installer/MainInstaller.sh
+/opt/BackupCamera/Installer/AutoBootSetup.sh
 tvservice -d edid
 edidparser edid
 
 #install Dashboard
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git)
-cd /usr/local/Epsilon-Dashboard
-./EpsilonDashboardSetup.sh
+git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/
+/opt/Epsilon-Dashboard/EpsilonDashboardSetup.sh
 
 #install Domovoi
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git)
+git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/
--- SecondaryInstallScript.sh
+++ SecondaryInstallScript.sh
@@ -1,36 +1,32 @@
 #!/bin/bash
 
-if [ $USER -ne "root"]
- 	then
- 	echo "Permission Denied"
- 	exit 1
+if [ $EUID -ne 0 ]; then
+	echo "Please run as root user"
+	exit 1
 fi
 
 ping -q -c1 google.com > /dev/null
  
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
 	echo "No Internet Connection...Aborting"
 	exit 1
 fi
 
-#install Epsilon-Onboard-Media-Control
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/BackupCamera.git)
-cd /usr/local/BackupCamera/Installer
-./MainInstaller.sh
-./AutoBootSetup.sh
-tvservice -d edid 
+#install BackupCamera
+git clone https://github.com/UCSolarCarTeam/BackupCamera.git /opt/
+/opt/BackupCamera/Installer/MainInstaller.sh
+/opt/BackupCamera/Installer/AutoBootSetup.sh
+tvservice -d edid
 edidparser edid
 
 #install Dashboard
-(cd /usr/local && git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git)
-cd /usr/local/Epsilon-Dashboard
-./EpsilonDashboardSetup.sh
+git clone https://github.com/UCSolarCarTeam/Epsilon-Dashboard.git /opt/
+/opt/Epsilon-Dashboard/EpsilonDashboardSetup.sh
 
-#install music-player
+#install Epsilon-Onboard-Media-Control
 cd /usr/local
 git clone https://github.com/UCSolarCarTeam/Epsilon-Onboard-Media-Control.git
 
 #install Domovoi
-cd /usr/local
-git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git
+git clone https://github.com/UCSolarCarTeam/Epsilon-Domovoi.git /opt/
+
